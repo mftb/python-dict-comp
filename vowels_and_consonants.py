c0292f91c206692e959b3f10322216d25d566f37
Matheus Boy <mboy@redhat.com>
2024-05-14 17:08:23 -0300

    adding a vowels and consonants examples

--- vowels_and_consonants.py
+++ vowels_and_consonants.py
@@ -1,14 +1,28 @@
+VOWELS = "aeiou"
+
+
 def vowels(word):
-    return {letter: letter in "aeiou" for letter in word}
+    return {letter: letter in VOWELS for letter in word}
 
 
 def consonants(word):
-    return {letter: letter not in "aeiou" for letter in word}
+    return {letter: letter not in VOWELS for letter in word}
 
 
 def main():
-    print(vowels("matheus"))
-    print(consonants("matheus"))
+    word = "matheus"
+    word_vowels = vowels(word)
+    word_consonants = consonants(word)
+    print(f"{word_vowels=}")
+    print("is a in word_vowels?", "a" in word_vowels)
+    print("is i in word_vowels?", "i" in word_vowels)
+    print("is a a vowel?", word_vowels["a"])
+    print("is m a vowel?", word_vowels["m"])
+    print(f"{word_consonants=}")
+    print("is m in word_consonants?", "m" in word_consonants)
+    print("is z in word_consonants?", "z" in word_consonants)
+    print("is a a consonant?", word_consonants["a"])
+    print("is m a consonant?", word_consonants["m"])
 
 
 if __name__ == "__main__":
--- vowels_and_consonants_filter.py
+++ vowels_and_consonants_filter.py
@@ -1,15 +1,27 @@
+VOWELS = "aeiou"
+
+
 def vowels(word):
-    return {letter: True for letter in word if letter in "aeiou"}
+    return {letter: True for letter in word if letter in VOWELS}
 
 
 def consonants(word):
-    return {letter: True for letter in word if letter not in "aeiou"}
+    return {letter: True for letter in word if letter not in VOWELS}
 
 
 def main():
-    print(vowels("matheus"))
-    print(consonants("matheus"))
-    
+    word = "matheus"
+    word_vowels = vowels(word)
+    word_consonants = consonants(word)
+    print(f"{word_vowels=}")
+    print("is a in word_vowels?", "a" in word_vowels)
+    print("is i in word_vowels?", "i" in word_vowels)
+    print("is a a vowel?", word_vowels["a"])
+    print(f"{word_consonants=}")
+    print("is m in word_consonants?", "m" in word_consonants)
+    print("is z in word_consonants?", "z" in word_consonants)
+    print("is m a consonant?", word_consonants["m"])
+
 
 if __name__ == "__main__":
     main()
